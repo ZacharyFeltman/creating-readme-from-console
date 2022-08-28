de4d816c56f8bd547cf6816dcb0b528ee3500585
ZacharyFeltman <zachary.feltman@gmail.com>
2022-08-28 12:45:22 -0500

    added questsions for readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,37 +1,67 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-
-//TODO: Create an array of questions for user input
+// TODO: Create an array of questions for user input
 // //Description, insall instructions, usage info, contirbution guidlines
 // // and test instructions.
-// const questions = [];
+const questions = [];
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      username: "username",
+      message: "Please enter your GitHub username:",
+    },
+    {
+      type: "input",
+      name: "first and last name",
+      message: "Please enter your first and last name:",
+    },
+    {
+      type: "input",
+      name: "project/name",
+      message:
+        "What is the project being installed? What is the projects name?",
+    },
+    {
+      type: "list",
+      name: "creation type",
+      message: "How do you prefer creating a Readme File?",
+      choices: "Console, Browser, Text Editor",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What is the usage for this application?",
+    },
+    {
+      type: "input",
+      name: "contributions",
+      message: "were there any other contributors to this project?",
+    },
+    {
+      type: "list",
+      name: "License",
+      message: "which license would you like to use?",
+      list: "",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "what tests were run for this project?",
+    },
+  ]);
+}
 
 // TODO: Create a function to write README file
-fs.appendFile('Readme.md', `${process.argv[2]}\n`, (error) =>
-    error ? console.error(error) : console.log('Commit Logged'))
-// function writeToFile(fileName, data) {}
+fs.appendFile("Readme.md", `${process.argv[2]}\n`, (error) =>
+  error ? console.error(error) : console.log("Commit Logged")
+);
+function writeToFile(fileName, data) {}
 
-// // TODO: Create a function to initialize app
-// function init() {}
- 
-// // Function call to initialize app
-// init();
-
-
-// // appendFile() takes in 3 arguments: path, data, and callback function
-//   // Ternary operator takes in a condition followed by a question mark (?)
-//   // then an expression to execute if the condition is truthy followed by a colon (:)
-//   // and finally the expression to execute if the condition is falsy.
-//   // This operator is frequently used as a shortcut for the if statement.
-//   err ? console.error(err) : console.log('Commit logged!')
-// );
-
-
-
-
-// const a = process.argv[2];
-// const b = process.argv[3];
+/// TODO: Create a function to initialize app
+function init() {}
 
 // // Using a conditional statement
 // if (a === b) {
@@ -48,4 +78,3 @@ fs.appendFile('Readme.md', `${process.argv[2]}\n`, (error) =>
 
 // // Comparing argument values directly
 // console.log(process.argv[2] === process.argv[3]);
-
