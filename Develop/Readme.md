ec9f00663a0f510ebd33126cb16db1a4d8cd78af
ZacharyFeltman <zachary.feltman@gmail.com>
2022-08-29 20:40:15 -0500

    git information displayed and organized in readme file

--- Develop/Readme.md
+++ Develop/Readme.md
@@ -1,1 +1,25 @@
-
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+# zach
+## Description
+  zach
+  
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+6. [Questions](#questions)
+## Installation
+zach
+## Usage
+zach
+## License
+This Project uses the GitHub licensing
+## Contributions
+zach
+## Tests
+zach
+## Questions
+[my GitHub profile](http://www.github.com/zachfeltman)
+You may contact me at zach with additional questions. 
--- Develop/index.js
+++ Develop/index.js
@@ -5,76 +5,93 @@ const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
 // //Description, insall instructions, usage info, contirbution guidlines
 // // and test instructions.
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "username",
+    message: "Please enter your GitHub username:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message:"enter your email",
+  },
+  {
+    type: "input",
+    name: "project_name",
+    message:
+      "What is the project being installed? What is the projects name?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "how would you describe this project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the usage for this application?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message:"what are the installation instructions?",
+  },
+  {
+    type: "input",
+    name: "contributions",
+    message: "were there any other contributors to this project?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "which license would you like to use?",
+    choices: ["GitHub","NPM","Bower"],
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "what tests were run for this project?",
+  },
+];
 function promptUser() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      username: "username",
-      message: "Please enter your GitHub username:",
-    },
-    {
-      type: "input",
-      name: "first and last name",
-      message: "Please enter your first and last name:",
-    },
-    {
-      type: "input",
-      name: "project/name",
-      message:
-        "What is the project being installed? What is the projects name?",
-    },
-    {
-      type: "list",
-      name: "creation type",
-      message: "How do you prefer creating a Readme File?",
-      choices: "Console, Browser, Text Editor",
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "What is the usage for this application?",
-    },
-    {
-      type: "input",
-      name: "contributions",
-      message: "were there any other contributors to this project?",
-    },
-    {
-      type: "list",
-      name: "License",
-      message: "which license would you like to use?",
-      list: "",
-    },
-    {
-      type: "input",
-      name: "tests",
-      message: "what tests were run for this project?",
-    },
-  ]);
-}
+  return inquirer.prompt(questions);
+};
 
-// TODO: Create a function to write README file
-fs.appendFile("Readme.md", `${process.argv[2]}\n`, (error) =>
+function writeToFile(data) {
+  fs.appendFile("readme.md", data, (error) =>
   error ? console.error(error) : console.log("Commit Logged")
 );
-function writeToFile(fileName, data) {}
+}
 
 /// TODO: Create a function to initialize app
 function init() {}
+ 
+promptUser().then((answers)=> {
+  writeToFile(`[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n`);
+  writeToFile("# " + answers.project_name + "\n");
+  writeToFile(`## Description
+  ${answers.description}
+  \n`)
+  writeToFile(`## Table of Contents\n`)
+  writeToFile(`1. [Installation](#installation)\n`)
+  writeToFile(`2. [Usage](#usage)\n`)
+  writeToFile(`3. [License](#license)\n`)
+  writeToFile(`4. [Contributing](#contributing)\n`)
+  writeToFile(`5. [Tests](#tests)\n`)
+  writeToFile(`6. [Questions](#questions)\n`)
+  writeToFile(`## Installation\n`)
+  writeToFile(`${answers.installation}\n`)
+  writeToFile(`## Usage\n`)
+  writeToFile(`${answers.usage}\n`)
+  writeToFile(`## License\n`)
+  writeToFile(`This Project uses the ${answers.license} licensing\n`)
+  writeToFile(`## Contributions\n`)
+  writeToFile(`${answers.contributions}\n`)
+  writeToFile(`## Tests\n`)
+  writeToFile(`${answers.tests}\n`)
+  writeToFile(`## Questions\n`)
+  writeToFile(`[my GitHub profile](http://www.github.com/${answers.username})\n`)
+  writeToFile(`You may contact me at ${answers.email} with additional questions. \n`)
+});
 
-// // Using a conditional statement
-// if (a === b) {
-//   console.log(true);
-// } else {
-//   console.log(false);
-// }
-
-// // Using a ternary operator
-// console.log(a === b ? true : false)
-
-// // Comparing variables
-// console.log(a === b);
-
-// // Comparing argument values directly
-// console.log(process.argv[2] === process.argv[3]);
